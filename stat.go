40f5ff5531ccda558d971e69d4095e219e29e19a
Andrew Gerrand <adg@google.com>
2010-11-23 16:59:11 +1100

    readme

---
+++ stat.go
@@ -0,0 +1,45 @@
+package stat
+
+import (
+	"log"
+	"time"
+)
+
+var In = make(chan string, 10000)
+
+type counter struct {
+	total, period, cycles int64
+}
+
+var counters = make(map[string]*counter)
+
+func Monitor(period int64) {
+	t := time.NewTicker(period)
+	for {
+		select {
+		case <-t.C:
+			output()
+		case s := <-In:
+			c, ok := counters[s]
+			if !ok {
+				c = &counter{}
+				counters[s] = c
+			}
+			c.period++
+		}
+	}
+}
+
+func output() {
+	for s, c := range counters {
+		c.total += c.period
+		c.cycles++
+		log.Printf("%s: %d total, %d last, %d avg", 
+			s,
+			c.total,
+			c.period,
+			c.total/c.cycles)
+		c.period = 0
+	}
+}
+

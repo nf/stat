5bab6077df81568e6f615f515c239a1a0fecaf84
Andrew Gerrand <nf@wh3rd.net>
2011-03-05 16:47:01 +1100

    updated

--- server/stats.go
+++ server/stats.go
@@ -16,41 +16,39 @@ var (
 	maxLen     = flag.Int("max", 60, "max points to retain")
 )
 
-type Server map[string][][2]int64
+type Server struct {
+	series map[string][][2]int64
+	start  int64
+	mu     sync.Mutex
+}
 
-func (s Server) Update(args *stat.Point, r *struct{}) os.Error {
-	lock.Lock()
-	t := (time.Nanoseconds() - start) / 100e6
-	k := args.Process + " " + args.Series
-	s[k] = append(s[k], [2]int64{t, args.Value})
-	if len(s[k]) > *maxLen {
-		s[k] = s[k][len(s[k])-*maxLen:]
+func NewServer() *Server {
+	return &Server{
+		series: make(map[string][][2]int64),
+		start:  time.Nanoseconds(),
 	}
-	lock.Unlock()
+}
+
+func (s *Server) Update(args *stat.Point, r *struct{}) os.Error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	// append point to series
+	key := args.Process + " " + args.Series
+	second := (time.Nanoseconds() - s.start) / 100e6
+	s.series[key] = append(s.series[key], [2]int64{second, args.Value})
+	// trim series to maxLen
+	if sk := s.series[key]; len(sk) > *maxLen {
+		sk = sk[len(sk)-*maxLen:]
+	}
 	return nil
 }
 
-var (
-	server = make(Server)
-	start  = time.Nanoseconds()
-	lock   sync.Mutex
-)
-
-func main() {
-	flag.Parse()
-	rpc.Register(server)
-	rpc.HandleHTTP()
-	http.HandleFunc("/", Static)
-	http.HandleFunc("/get", Get)
-	http.ListenAndServe(*listenAddr, nil)
-}
-
-func Get(w http.ResponseWriter, r *http.Request) {
+func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
 	w.SetHeader("Content-Type", "application/json")
-	lock.Lock()
 	e := json.NewEncoder(w)
-	e.Encode(server)
-	lock.Unlock()
+	e.Encode(s.series)
 }
 
 func Static(w http.ResponseWriter, r *http.Request) {
@@ -62,4 +60,14 @@ func Static(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	http.ServeFile(w, r, "static/"+filename)
+}
+
+func main() {
+	flag.Parse()
+	server := NewServer()
+	rpc.Register(server)
+	rpc.HandleHTTP()
+	http.HandleFunc("/", Static)
+	http.Handle("/get", server)
+	http.ListenAndServe(*listenAddr, nil)
 }
